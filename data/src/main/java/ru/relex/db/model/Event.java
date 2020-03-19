1972e2df04ba2f544d759027bc00eefd33662a1d
Maksim Borisov <lixard870@gmail.com>
2020-03-20 02:28:06 +0300

    Add the remaining database models

---
+++ data/src/main/java/ru/relex/db/model/Event.java
@@ -0,0 +1,97 @@
+package ru.relex.db.model;
+
+import java.sql.Timestamp;
+
+public class Event {
+    private int eventId;
+    private String eventName;
+    //TODO Сделать enum для типов ивентов и поменять здесь int на него
+    private int eventType;
+    private int conferenceId;
+    private String htmlDescription;
+    private String location;
+    private Timestamp timeStart;
+    private Timestamp timeEnd;
+    private int createdBy;
+    private boolean deleted;
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public int getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(int eventType) {
+        this.eventType = eventType;
+    }
+
+    public int getConferenceId() {
+        return conferenceId;
+    }
+
+    public void setConferenceId(int conferenceId) {
+        this.conferenceId = conferenceId;
+    }
+
+    public String getHtmlDescription() {
+        return htmlDescription;
+    }
+
+    public void setHtmlDescription(String htmlDescription) {
+        this.htmlDescription = htmlDescription;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Timestamp getTimeStart() {
+        return timeStart;
+    }
+
+    public void setTimeStart(Timestamp timeStart) {
+        this.timeStart = timeStart;
+    }
+
+    public Timestamp getTimeEnd() {
+        return timeEnd;
+    }
+
+    public void setTimeEnd(Timestamp timeEnd) {
+        this.timeEnd = timeEnd;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+}
---
+++ data/src/main/java/ru/relex/db/model/EventSpeaker.java
@@ -0,0 +1,51 @@
+package ru.relex.db.model;
+
+import java.sql.Timestamp;
+
+public class EventSpeaker {
+    private int userId;
+    private int eventId;
+    private int createdBy;
+    private Timestamp createdAt;
+    private boolean deleted;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+}

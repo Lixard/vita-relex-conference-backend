1bb1025034e45a9d53474c83a75c95721b09af6c
Vlad Peltikhin <peltikhin.vladik@gmail.com>
2020-03-20 01:39:36 +0300

    update UserMapper

--- data/src/main/java/ru/relex/db/mapper/UserMapper.java
+++ data/src/main/java/ru/relex/db/mapper/UserMapper.java
@@ -1,7 +1,58 @@
 package ru.relex.db.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
+import ru.relex.db.model.User;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
+    @Select(
+            "SELECT " +
+                    "user_id, " +
+                    "username, " +
+                    "first_name, " +
+                    "last_name, " +
+                    "email, " +
+                    "password, " +
+                    "role, " +
+                    "deleted " +
+                    "FROM users " +
+                    "WHERE #{search} IS NULL " +
+                    "OR CONCAT_WS('$', first_name, last_name, username) LIKE CONCAT('%', #{search:VARCHAR}, '%')"
+    )
+    List<User> getUsers(@Param("search") String search);
+
+    @Select(
+            "SELECT " +
+                "user_id, " +
+                "username, " +
+                "first_name, " +
+                "last_name, " +
+                "email, " +
+                "password, " +
+                "role, " +
+                "deleted " +
+                "FROM users " +
+                "WHERE user_id = #{id} "
+    )
+    User findById(@Param("id") int id);
+
+    @Update(
+            "UPDATE users" +
+                    "SET username = #{username}," +
+                    "first_name = #{firstName}" +
+                    "last_name = #{lastName}" +
+                    "email = #{email}" +
+                    "password = #{password}" +
+                    "role = #{role}" +
+                    "deleted = #{deleted}" +
+                    "WHERE user_id = #{userId}"
+    )
+    void update(User user);
+
+    @Delete(
+            "DELETE FROM users WHERE user_id = #{id}"
+    )
+    void delete(@Param("id") int id);
 }

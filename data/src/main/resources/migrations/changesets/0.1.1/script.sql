4b28dc7cdb8a364bd162dc2c8ed46c3a0c84266c
Maksim Borisov <lixard870@gmail.com>
2020-03-20 14:35:10 +0300

    Add new migration, insert enum values into roles and event_type tables

---
+++ data/src/main/resources/migrations/changesets/0.1.1/script.sql
@@ -0,0 +1,11 @@
+INSERT INTO roles(role_id, name)
+VALUES (1, 'ADMIN'),
+       (2, 'USER'),
+       (3, 'COMPANY_ACCOUNT');
+
+INSERT INTO event_types(event_type_id, name)
+VALUES (1, 'MEETUP'),
+       (2, 'LECTURE'),
+       (3, 'SEMINAR'),
+       (4, 'PRODUCT_PRESENTATION'),
+       (5, 'ENTERTAINING_EVENT');
